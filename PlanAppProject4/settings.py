a3e8daaaaf3a887b21a2868224cab8337af2e706
nicolasmarat9 <62978650+nicolasmarat9@users.noreply.github.com>
2021-01-23 14:38:39 +0100

    Update settings.py

--- PlanAppProject4/settings.py
+++ PlanAppProject4/settings.py
@@ -109,11 +109,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "assets")
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'    
+ 
 ]
 
 PWA_SERVICE_WORKER_PATH = os.path.join(BASE_DIR, 'serviceworker.js')
