752f4a77634c3f1c7a16ec84defbd6a99729ac49
nicolasmarat9 <62978650+nicolasmarat9@users.noreply.github.com>
2021-01-23 19:32:22 +0100

    Update settings.py

--- PlanAppProject4/settings.py
+++ PlanAppProject4/settings.py
@@ -110,6 +110,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'assets/images')
 if DEBUG:
 
     STATICFILES_DIRS = [
