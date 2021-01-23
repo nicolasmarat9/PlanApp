928815ded0984e87ebf82ee3e4d7234027c865ba
nicolasmarat9 <62978650+nicolasmarat9@users.noreply.github.com>
2021-01-23 18:17:05 +0100

    Update urls.py

--- PlanAppProject4/urls.py
+++ PlanAppProject4/urls.py
@@ -6,7 +6,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.static import serve
 from django.conf.urls import url
 from django.conf import settings
-
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("Calendar.urls")),
