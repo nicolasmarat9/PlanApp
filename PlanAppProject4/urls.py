36181d4a806848992f78e1bf7d24bc838836b947
nicolasmarat9 <62978650+nicolasmarat9@users.noreply.github.com>
2021-01-23 19:41:36 +0100

    Update urls.py

--- PlanAppProject4/urls.py
+++ PlanAppProject4/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.static import serve
 from django.conf.urls import url
-from django.conf import settings
+import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
