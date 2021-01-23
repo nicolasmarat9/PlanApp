6f9e40a73bd327b7358fdca4a8c74ccae7ccba4b
nicolasmarat9 <62978650+nicolasmarat9@users.noreply.github.com>
2021-01-23 19:04:23 +0100

    Update urls.py

--- PlanAppProject4/urls.py
+++ PlanAppProject4/urls.py
@@ -12,7 +12,8 @@ urlpatterns = [
     path('', include("Calendar.urls")),
     path('login/', include("login.urls")),
     path('', include('pwa.urls')),
-   
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),      
 ]
 
-urlpatterns += staticfiles_urlpatterns() 
+urlpatterns += staticfiles_urlpatterns(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT) 
