7b95403189e67bb320c000b2b7a4b99af33370b3
nicolasmarat9 <62978650+nicolasmarat9@users.noreply.github.com>
2021-01-24 12:24:39 +0100

    Update settings.py

--- PlanAppProject4/settings.py
+++ PlanAppProject4/settings.py
@@ -131,7 +131,7 @@ PWA_APP_BACKGROUND_COLOR = '#ffffff'
 PWA_APP_DISPLAY = 'standalone'
 PWA_APP_SCOPE = '/'
 PWA_APP_ORIENTATION = 'any'
-PWA_APP_START_URL = '/planappnicolasclimb.herokuapp.com'
+PWA_APP_START_URL = '/home'
 PWA_APP_STATUS_BAR_COLOR = 'default'
 PWA_APP_ICONS = [
     {
